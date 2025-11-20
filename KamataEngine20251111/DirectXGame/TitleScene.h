18f40479307d82b5fba3b439041f557da644c8c5
Sororo0403 <k024g0045@g.neec.ac.jp>
2025-11-20 12:45:45 +0900

    feat: TitleScene を追加

---
+++ KamataEngine20251111/DirectXGame/TitleScene.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "IScene.h"
+
+class TitleScene : public IScene {
+public:
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize() override;
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update() override;
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw() override;
+};
--- KamataEngine20251111/DirectXGame/main.cpp
+++ KamataEngine20251111/DirectXGame/main.cpp
@@ -1,25 +1,36 @@
-#include <Windows.h>
+#include "SceneManager.h"
+#include "TitleScene.h"
 #include <KamataEngine.h>
+#include <Windows.h>
 
 using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-    // エンジンの初期化
+	// エンジンの初期化
 	KamataEngine::Initialize(L"GameProject");
 
 	// DirectXCommon のインスタンス取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
+	// 最初のシーンを TitleScene に設定
+	SceneManager sceneManager(std::make_unique<TitleScene>());
+
 	// メインループ
 	while (true) {
-		// エンジン更新
+		// エンジン更新（入力更新など）
 		if (KamataEngine::Update()) {
 			break;
 		}
 
+		// シーン更新
+		sceneManager.Update();
+
 		// 描画開始
 		dxCommon->PreDraw();
+
+		// シーン描画
+		sceneManager.Draw();
 
 		// 描画終了
 		dxCommon->PostDraw();
